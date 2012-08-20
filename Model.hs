f522c38958634ae7a9f7ec599cd28c27c0ad4d93
seizans <seizans@gmail.com>
2012-08-21 02:18:38 +0900

    config/models に DSL を書いて新しい Model を定義する.
    - 真似て書けばOK.
    - DB の DDL も自動生成し、ビルド時に Migration される.
    - Enum用途のフィールド(カラム)を作成することもできる.
      - Model.hs にデータ型を定義し、derivePersistField すればOK.

--- Model.hs
+++ Model.hs
@@ -5,6 +5,9 @@ import Yesod
 import Data.Text (Text)
 import Database.Persist.Quasi
 
+data Blood = A | B | O | AB
+  deriving (Show, Read, Eq, Ord, Enum, Bounded)
+derivePersistField "Blood"
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
